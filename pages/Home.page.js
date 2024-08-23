406b9c8fec5d4d00d72af848cfdbf3bdf4473e1f
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-08-24 02:34:20 +0300

    test case 2(v. 2.0)
    need to be upgraded(POM)

---
+++ pages/Home.page.js
@@ -0,0 +1,48 @@
+class SignUpPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  get loginInput() {
+    return this.page.locator("#user_login");
+  }
+
+  get passwordInput() {
+    return this.page.locator("#user_password");
+  }
+
+  get passwordConfirmationInput() {
+    return this.page.locator("#user_password_confirmation");
+  }
+
+  get firstNameInput() {
+    return this.page.locator("#user_firstname");
+  }
+
+  get lastNameInput() {
+    return this.page.locator("#user_lastname");
+  }
+
+  get emailInput() {
+    return this.page.locator("#user_mail");
+  }
+
+  get submitButton() {
+    return this.page.locator('form>input[type="submit"]');
+  }
+
+  async fillForm({ login, password, firstName, lastName, email }) {
+    await this.loginInput.fill(login);
+    await this.passwordInput.fill(password);
+    await this.passwordConfirmationInput.fill(password);
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.emailInput.fill(email);
+  }
+
+  async submit() {
+    await this.submitButton.click();
+  }
+}
+
+export default SignUpPage;
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,39 +1,41 @@
 import { test, expect } from "@playwright/test";
+import SignUpPage from "../pages/Home.page.js";
 
-// test case 1
-test("Registration of a user on the website using valid data", async ({
-  page,
-}) => {
-  await page.goto("https://www.redmine.org/");
+// // test case 1
+// test("Registration of a user on the website using valid data", async ({
+//   page,
+// }) => {
+//   await page.goto("https://www.redmine.org/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("Overview - Redmine");
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle("Overview - Redmine");
 
-  // Клік по лінку за допомогою CSS селектора
-  await page.locator("#account > ul > li > a.register").click();
-  await page.locator("#user_login").click();
-  await page.locator("#user_login").fill("loginforrkachuk123");
-  await page.locator("#user_password").fill("passwordfortesting");
-  await page.locator("#user_password_confirmation").fill("passwordfortesting");
-  await page.locator("#user_firstname").fill("Tom");
-  await page.locator("#user_lastname").fill("Anderson");
-  await page.locator("#user_mail").fill("mailfotaqa123@gmail.com");
-  await page.locator('form>input[type="submit"]').click();
-  // page.locator("#flash_notice");
-  // await expect(page).toHaveURL("https://www.redmine.org/login");
-  // Перевірка, чи з'явилося повідомлення про успішну реєстрацію
-  const flashNotice = page.locator("#flash_notice");
-  await expect(flashNotice).toHaveAttribute("class", "flash notice");
+//   // Клік по лінку за допомогою CSS селектора
+//   await page.locator("#account > ul > li > a.register").click();
+//   await page.locator("#user_login").click();
+//   await page.locator("#user_login").fill("loginforrkachuk123");
+//   await page.locator("#user_password").fill("passwordfortesting");
+//   await page.locator("#user_password_confirmation").fill("passwordfortesting");
+//   await page.locator("#user_firstname").fill("Tom");
+//   await page.locator("#user_lastname").fill("Anderson");
+//   await page.locator("#user_mail").fill("mailfotaqa123@gmail.com");
+//   await page.locator('form>input[type="submit"]').click();
+//   // page.locator("#flash_notice");
+//   // await expect(page).toHaveURL("https://www.redmine.org/login");
+//   // Перевірка, чи з'явилося повідомлення про успішну реєстрацію
+//   const flashNotice = page.locator("#flash_notice");
+//   await expect(flashNotice).toHaveAttribute("class", "flash notice");
 
-  // Перевірка, чи відбулося перенаправлення на сторінку входу
-  await expect(page).toHaveURL("https://www.redmine.org/login");
-});
+//   // Перевірка, чи відбулося перенаправлення на сторінку входу
+//   await expect(page).toHaveURL("https://www.redmine.org/login");
+// });
 
 // test case 2
 
 test("Registration of a user on the website using invalid email", async ({
   page,
 }) => {
+  const signUpPage = new SignUpPage(page);
   await page.goto("https://www.redmine.org/");
 
   // Expect a title "to contain" a substring.
@@ -41,14 +43,24 @@ test("Registration of a user on the website using invalid email", async ({
 
   // Клік по лінку за допомогою CSS селектора
   await page.locator("#account > ul > li > a.register").click();
-  await page.locator("#user_login").click();
-  await page.locator("#user_login").fill("loginforrkachuk2");
-  await page.locator("#user_password").fill("passwordfortesting");
-  await page.locator("#user_password_confirmation").fill("passwordfortesting");
-  await page.locator("#user_firstname").fill("Tom");
-  await page.locator("#user_lastname").fill("Anderson");
-  await page.locator("#user_mail").fill("mailfotaqa1@qwe");
-  await page.locator('form>input[type="submit"]').click();
+  // // await page.locator("#user_login").click();
+  // // await page.locator("#user_login").fill("loginforrkachuk2");
+  // // await page.locator("#user_password").fill("passwordfortesting");
+  // // await page.locator("#user_password_confirmation").fill("passwordfortesting");
+  // // await page.locator("#user_firstname").fill("Tom");
+  // // await page.locator("#user_lastname").fill("Anderson");
+  // // await page.locator("#user_mail").fill("mailfotaqa1@qwe");
+  // // await page.locator('form>input[type="submit"]').click();
+
+  await signUpPage.fillForm({
+    login: "loginforrkachuk2",
+    password: "passwordfortesting",
+    firstName: "Tom",
+    lastName: "Anderson",
+    email: "mailfotaqa1@qwe",
+  });
+
+  await signUpPage.submit();
 
   // Перевірка, що елемент має потрібний текст
   const errorMessage = page.locator("#errorExplanation>ul>li");
@@ -58,113 +70,113 @@ test("Registration of a user on the website using invalid email", async ({
   await expect(page).toHaveURL("https://www.redmine.org/account/register");
 });
 
-//test case 3
-test("Check the documentation on the User's Guide page", async ({ page }) => {
-  await page.goto("https://www.redmine.org/");
+// //test case 3
+// test("Check the documentation on the User's Guide page", async ({ page }) => {
+//   await page.goto("https://www.redmine.org/");
 
-  //знаходжу елемент, провіряю чи видимий і скролю
-  const docsSection = page.locator('h2:has-text("Documentation")');
-  await expect(docsSection).toBeVisible();
-  await docsSection.scrollIntoViewIfNeeded();
+//   //знаходжу елемент, провіряю чи видимий і скролю
+//   const docsSection = page.locator('h2:has-text("Documentation")');
+//   await expect(docsSection).toBeVisible();
+//   await docsSection.scrollIntoViewIfNeeded();
 
-  const guideLink = page.locator(
-    '#content ul>li>a[href="/projects/redmine/wiki/Guide"]'
-  );
-  await guideLink.click();
+//   const guideLink = page.locator(
+//     '#content ul>li>a[href="/projects/redmine/wiki/Guide"]'
+//   );
+//   await guideLink.click();
 
-  // Чекаємо певний час перед повторною перевіркою
-  await page.waitForTimeout(500);
+//   // Чекаємо певний час перед повторною перевіркою
+//   await page.waitForTimeout(500);
 
-  // Знаходимо зовнішній iframe
-  const outerFrame = page.frameLocator("#aswift_6");
+//   // Знаходимо зовнішній iframe
+//   const outerFrame = page.frameLocator("#aswift_6");
 
-  // Всередині зовнішнього iframe знаходимо внутрішній iframe
-  const innerFrame = outerFrame.frameLocator("#ad_iframe");
+//   // Всередині зовнішнього iframe знаходимо внутрішній iframe
+//   const innerFrame = outerFrame.frameLocator("#ad_iframe");
 
-  // У внутрішньому iframe знаходимо кнопку і клікаємо по ній
-  const dismissButton = innerFrame.locator("#dismiss-button");
-  // let dismissButton = null;
-  // if (innerFrame) {
-  //   dismissButton = innerFrame.locator("#dismiss-button");
-  // } else {
-  //   dismissButton = outerFrame.locator("#dismiss-button");
-  // }
+//   // У внутрішньому iframe знаходимо кнопку і клікаємо по ній
+//   const dismissButton = innerFrame.locator("#dismiss-button");
+//   // let dismissButton = null;
+//   // if (innerFrame) {
+//   //   dismissButton = innerFrame.locator("#dismiss-button");
+//   // } else {
+//   //   dismissButton = outerFrame.locator("#dismiss-button");
+//   // }
 
-  if (await dismissButton.count()) await dismissButton.click();
-  // if (await expect(dismissButton).toBeVisible()) {
-  //   await dismissButton.click();
-  // }
+//   if (await dismissButton.count()) await dismissButton.click();
+//   // if (await expect(dismissButton).toBeVisible()) {
+//   //   await dismissButton.click();
+//   // }
 
-  //знаходжу елемент, провіряю чи видимий і скролю
-  const guideSection = page.locator('h2:has-text("User guide")');
-  // await expect(guideSection).toBeVisible();
-  await guideSection.scrollIntoViewIfNeeded();
+//   //знаходжу елемент, провіряю чи видимий і скролю
+//   const guideSection = page.locator('h2:has-text("User guide")');
+//   // await expect(guideSection).toBeVisible();
+//   await guideSection.scrollIntoViewIfNeeded();
 
-  const getStartedLink = page.locator(
-    'a[href="/projects/redmine/wiki/Getting_Started"]'
-  );
-  await getStartedLink.click();
+//   const getStartedLink = page.locator(
+//     'a[href="/projects/redmine/wiki/Getting_Started"]'
+//   );
+//   await getStartedLink.click();
 
-  await expect(page).toHaveURL(
-    "https://www.redmine.org/projects/redmine/wiki/Getting_Started"
-  );
+//   await expect(page).toHaveURL(
+//     "https://www.redmine.org/projects/redmine/wiki/Getting_Started"
+//   );
 
-  const expectedText = "Step One -- Creating a project¶";
+//   const expectedText = "Step One -- Creating a project¶";
 
-  const stepOne = page.locator("#content > div.wiki-page > h2:nth-child(6)");
-  await expect(stepOne).toHaveText(expectedText);
-});
+//   const stepOne = page.locator("#content > div.wiki-page > h2:nth-child(6)");
+//   await expect(stepOne).toHaveText(expectedText);
+// });
 
-// test case 4
-test('Filtering the data on the "issues" page', async ({ page }) => {
-  await page.goto("https://www.redmine.org/");
+// // test case 4
+// test('Filtering the data on the "issues" page', async ({ page }) => {
+//   await page.goto("https://www.redmine.org/");
 
-  //знаходжу елемент, провіряю чи видимий і скролю
-  const issuelsLink = page.locator('#main-menu a[class="issues"]');
-  await expect(issuelsLink).toBeVisible();
-  await issuelsLink.click();
+//   //знаходжу елемент, провіряю чи видимий і скролю
+//   const issuelsLink = page.locator('#main-menu a[class="issues"]');
+//   await expect(issuelsLink).toBeVisible();
+//   await issuelsLink.click();
 
-  ///////////////////////////////////////////////////////////////
-  await page.waitForTimeout(500);
+//   ///////////////////////////////////////////////////////////////
+//   await page.waitForTimeout(500);
 
-  // Знаходимо зовнішній iframe
-  const outerFrame = page.frameLocator("#aswift_6");
-  // Всередині зовнішнього iframe знаходимо внутрішній iframe
-  const innerFrame = outerFrame.frameLocator("#ad_iframe");
+//   // Знаходимо зовнішній iframe
+//   const outerFrame = page.frameLocator("#aswift_6");
+//   // Всередині зовнішнього iframe знаходимо внутрішній iframe
+//   const innerFrame = outerFrame.frameLocator("#ad_iframe");
 
-  // У внутрішньому iframe знаходимо кнопку і клікаємо по ній
-  const dismissButton = innerFrame.locator("#dismiss-button");
+//   // У внутрішньому iframe знаходимо кнопку і клікаємо по ній
+//   const dismissButton = innerFrame.locator("#dismiss-button");
 
-  if (await dismissButton.count()) await dismissButton.click();
-  ////////////////////////////////////////////////////////////////
-  await expect(page).toHaveURL(
-    "https://www.redmine.org/projects/redmine/issues"
-  );
+//   if (await dismissButton.count()) await dismissButton.click();
+//   ////////////////////////////////////////////////////////////////
+//   await expect(page).toHaveURL(
+//     "https://www.redmine.org/projects/redmine/issues"
+//   );
 
-  const filterSelect = page.locator("#add_filter_select");
-  await filterSelect.selectOption("tracker_id");
+//   const filterSelect = page.locator("#add_filter_select");
+//   await filterSelect.selectOption("tracker_id");
 
-  await page.waitForTimeout(500);
+//   await page.waitForTimeout(500);
 
-  const applyLink = page.locator("#query_form_content+p>a:first-child");
-  await applyLink.click();
+//   const applyLink = page.locator("#query_form_content+p>a:first-child");
+//   await applyLink.click();
 
-  await page.waitForTimeout(500);
+//   await page.waitForTimeout(500);
 
-  const rows = page.locator("tbody > tr td.tracker");
-  console.log("rows", rows);
-  const rowCount = await rows.count();
-  console.log("rowsCount", rowCount);
+//   const rows = page.locator("tbody > tr td.tracker");
+//   console.log("rows", rows);
+//   const rowCount = await rows.count();
+//   console.log("rowsCount", rowCount);
 
-  if (rowCount === 0) {
-    throw new Error("No rows found in the table");
-  }
+//   if (rowCount === 0) {
+//     throw new Error("No rows found in the table");
+//   }
 
-  for (let i = 0; i < rowCount; i++) {
-    const trackerText = await rows.nth(i).textContent();
-    expect(trackerText.trim()).toBe("Defect");
-  }
-});
+//   for (let i = 0; i < rowCount; i++) {
+//     const trackerText = await rows.nth(i).textContent();
+//     expect(trackerText.trim()).toBe("Defect");
+//   }
+// });
 
 // // test case 5
 // test("Changing the password on the user page", async ({ page }) => {
